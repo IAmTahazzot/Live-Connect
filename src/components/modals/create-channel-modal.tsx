a750f939e7ec6dae8e545061d6fc12cc94856df0
Tahazzot <iamtahazzot@gmail.com>
2024-05-08 21:52:30 +0600

    refactor: Improve channel create modal design and logic

--- src/components/modals/create-channel-modal.tsx
+++ src/components/modals/create-channel-modal.tsx
@@ -4,10 +4,8 @@ import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import qs from 'query-string'
-
 import { Dialog, DialogTitle, DialogContent, DialogHeader, DialogFooter } from '@/components/ui/dialog'
-
-import { Form, FormControl, FormField, FormLabel, FormItem, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormLabel, FormItem } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useEffect, useState } from 'react'
@@ -15,7 +13,8 @@ import axios from 'axios'
 import { useParams, useRouter } from 'next/navigation'
 import { MODAL_TYPES, useModal } from '@/hooks/use-modal-store'
 import { ChannelType } from '@prisma/client'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Hash, MicIcon, VideoIcon } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 const formSchema = z.object({
   name: z
@@ -85,39 +84,81 @@ const CreateChannelModal = () => {
     close()
   }
 
+  const renderRadioCircle = (selected: boolean) => {
+    return (
+      <svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+          fill="currentColor"></path>
+        {selected && <circle cx="12" cy="12" r="5" fill="currentColor"></circle>}
+      </svg>
+    )
+  }
+
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="bg-white text-black dark:bg-[#313338] dark:text-[#C4C9CE] p-0 overflow-hidden max-w-[440px] rounded-[5px]">
+      <DialogContent className="bg-white text-black dark:bg-[#313338] dark:text-[#C4C9CE] p-0 overflow-hidden max-w-[450px] rounded-[5px]">
         <DialogHeader className="pt-5 px-4">
-          <DialogTitle className="text-2xl dark:text-slate-200 text-center font-medium">Create a Channel</DialogTitle>
+          <DialogTitle className="text-2xl dark:text-slate-200 font-medium">Create a Channel</DialogTitle>
         </DialogHeader>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-4 px-4">
-              <FormField
-                control={form.control}
-                name="type"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Channel Type</FormLabel>
-                    <Select disabled={isLoading} onValueChange={field.onChange} defaultValue={field.value}>
-                      <FormControl>
-                        <SelectTrigger className="bg-zinc-300 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none">
-                          <SelectValue placeholder="Select a channel type" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {Object.values(ChannelType).map(type => (
-                          <SelectItem key={type} value={type} className="capitalize">
-                            {type.toLowerCase()}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+              <div className="space-y-2">
+                <div
+                  className={cn(
+                    'grid grid-cols-[40px_1fr_40px] items-center justify-items-center dark:bg-[#2b2d31] dark:hover:bg-[#3c3e44b9] rounded-sm p-2 select-none cursor-pointer',
+                    form.watch('type') === ChannelType.TEXT && 'dark:bg-[#4e505899]'
+                  )}
+                  onClick={() => {
+                    form.setValue('type', ChannelType.TEXT)
+                  }}>
+                  <Hash size={20} className="text-white dark:text-white" />
+                  <div className="justify-self-start pl-1">
+                    <h3 className="text-white">Text</h3>
+                    <p className="text-sm text-ellipsis whitespace-nowrap overflow-hidden">
+                      Send messages, images, GIFs, emoji, opinions and puns
+                    </p>
+                  </div>
+                  <div>{renderRadioCircle(form.watch('type') === ChannelType.TEXT)}</div>
+                </div>
+                <div
+                  className={cn(
+                    'grid grid-cols-[40px_1fr_40px] items-center justify-items-center dark:bg-[#2b2d31] dark:hover:bg-[#3c3e44b9] rounded-sm p-2 select-none cursor-pointer',
+                    form.watch('type') === ChannelType.AUDIO && 'dark:bg-[#4e505899]'
+                  )}
+                  onClick={() => {
+                    form.setValue('type', ChannelType.AUDIO)
+                  }}>
+                  <MicIcon size={20} className="text-white dark:text-white" />
+                  <div className="justify-self-start pl-1">
+                    <h3 className="text-white">Voice</h3>
+                    <p className="text-sm text-ellipsis whitespace-nowrap overflow-hidden">
+                      Hang out together with voice, video and screen sharing
+                    </p>
+                  </div>
+                  <div>{renderRadioCircle(form.watch('type') === ChannelType.AUDIO)}</div>
+                </div>
+                <div
+                  className={cn(
+                    'grid grid-cols-[40px_1fr_40px] items-center justify-items-center dark:bg-[#2b2d31] dark:hover:bg-[#3c3e44b9] rounded-sm p-2 select-none cursor-pointer',
+                    form.watch('type') === ChannelType.VIDEO && 'dark:bg-[#4e505899]'
+                  )}
+                  onClick={() => {
+                    form.setValue('type', ChannelType.VIDEO)
+                  }}>
+                  <VideoIcon size={20} className="text-white dark:text-white" />
+                  <div className="justify-self-start pl-1">
+                    <h3 className="text-white">Video</h3>
+                    <p className="text-sm text-ellipsis whitespace-nowrap overflow-hidden">
+                      Face time with your friends, family and colleagues
+                    </p>
+                  </div>
+                  <div>{renderRadioCircle(form.watch('type') === ChannelType.VIDEO)}</div>
+                </div>
+              </div>
 
               <FormField
                 control={form.control}
@@ -160,7 +201,7 @@ const CreateChannelModal = () => {
                 variant="primary"
                 className={'rounded-[3px] px-6 bg-[--color-primary]'}
                 disabled={isLoading}>
-                Create
+                {form.formState.isSubmitting ? 'Creating...' : 'Create'}
               </Button>
             </DialogFooter>
           </form>
