b20abfe0e1f5f8053e1308d48f3415107b13899a
Tahazzot <iamtahazzot@gmail.com>
2024-05-15 10:01:37 +0600

    [WARNING]: DevServer bind problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/icon', '/api/uploadthing', '/invite/(.*)', '/api/servers', '/api/socket/io'],
+  publicRoutes: ['/', '/icon', '/api/uploadthing', '/invite/(.*)', '/api/servers'],
+  ignoredRoutes: ['/((?!api|trpc))(_next.*|.+.[w]+$)', '/api/socket/io'],
 })
 
 export const config = {
