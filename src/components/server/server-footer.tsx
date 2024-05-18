9988a8c0d88d5afa1112808cde4442e70aecb193
Tahazzot <iamtahazzot@gmail.com>
2024-05-18 14:01:51 +0600

    feat: server footer on hover status change

--- src/components/server/server-footer.tsx
+++ src/components/server/server-footer.tsx
@@ -12,15 +12,22 @@ export const ServerFooter = ({ profile }: ServerFooterProps) => {
   const { onOpen } = usePanel()
 
   return (
-    <div className="grid grid-cols-[minmax(100px,150px)_auto] items-center dark:bg-[hsla(var(--background-deep-dark),.8)] px-2 py-[6px]">
+    <div className="grid grid-cols-[minmax(100px,150px)_auto] items-center dark:bg-[hsla(var(--background-deep-dark),.8)] px-2 py-[6px] group">
       <div className="flex items-center gap-2 dark:hover:bg-[hsla(var(--background-modifier-selected)/.3)] rounded-sm p-[2px] select-none cursor-pointer overflow-hidden">
         <div className="w-8 h-8 relative rounded-full shrink-0">
-          <Image src={profile.imageUrl} fill className='rounded-full' alt="user profile picture" priority={true} />
+          <Image src={profile.imageUrl} fill className="rounded-full" alt="user profile picture" priority={true} />
           <div className=" box-content absolute h-[10px] w-[10px] rounded-full bg-emerald-500 -bottom-[2px] -right-1 z-10 border-solid border-[4px] dark:border-[#212226]"></div>
         </div>
         <div className="flex flex-col gap-0">
           <h4 className="text-sm mb-0 text-ellipsis whitespace-nowrap max-w-[110px]">{profile.name}</h4>
-          <p className="text-[12px] text-gray-400 m-0">Online</p>
+          <div className="relative h-4 overflow-hidden">
+            <p className="text-[12px] text-gray-400 m-0 transition-transform duration-200 group-hover:-translate-y-4">
+              Online
+            </p>
+            <p className="text-[12px] text-gray-400 m-0 transition-transform duration-200 group-hover:-translate-y-5">
+              {profile.email.split('@')[0]}
+            </p>
+          </div>
         </div>
       </div>
       <div className="grid content-center justify-items-end pr-2">
